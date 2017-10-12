5f9e876692ff469cdb51f7d4a979b6258b546702
Matheus Paiva <mmccpp@gmail.com>
2017-10-12 03:02:41 -0300

    added jwt layer

--- src/controllers/root/root.index.js
+++ src/controllers/root/root.index.js
@@ -1,4 +1,12 @@
+import jwt from 'jsonwebtoken'
+
+const { JWT_SECRET } = process.env
+
 /**
  * GET /
  */
-export const get = (req, res) => res.json({ olar: 'olar' })
+export const authenticate = (req, res) => {
+  const token = jwt.sign({ foo: 'bar' }, JWT_SECRET);
+  return res.json({ token })
+}
+  export const get = (req, res) => res.json({ olar: 'olar' })
--- src/routes.js
+++ src/routes.js
@@ -1,10 +1,21 @@
 import { Router } from 'express'
+import jwtValidator from 'express-jwt'
+import errors from 'throw.js'
 
 import * as controllers from './controllers'
 
+const { JWT_SECRET } = process.env
+
 const routes = Router()
 
+routes.use(jwtValidator({ secret: JWT_SECRET }).unless({ path: [
+  '/token'
+] }))
+routes.use((err, req, res, next) => err.name === 'UnauthorizedError' && next(new errors.Unauthorized()))
+
 routes.get('/', controllers.root.get)
+routes.get('/token', controllers.root.authenticate)
 routes.get('/list', controllers.list.get)
+
 
 export default routes
