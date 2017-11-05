60b024e65a279f468cb5a185a4795d90c3c60641
Matheus Paiva <mmccpp@gmail.com>
2017-11-04 23:53:33 -0200

    add possibility to login to db without user and password

--- src/model/connectToDb.js
+++ src/model/connectToDb.js
@@ -1,25 +1,11 @@
 import mongoose from 'mongoose'
 import bluebird from 'bluebird'
 
-/**
- * String
-    lowercase: boolean, whether to always call .toLowerCase() on the value
-    uppercase: boolean, whether to always call .toUpperCase() on the value
-    trim: boolean, whether to always call .trim() on the value
-    match: RegExp, creates a validator that checks if the value matches the given regular expression
-    enum: Array, creates a validator that checks if the value is in the given array.
-
-  Number
-    min: Number, creates a validator that checks if the value is greater than or equal to the given minimum.
-    max: Number, creates a validator that checks if the value is less than or equal to the given maximum.
-
-  Date
-    min: Date
-    max: Date
- */
-
 mongoose.Promise = bluebird
 
-const connectToDb = async (dbUser, dbPassword, dbAddress) => mongoose.connect(`mongodb://${dbUser}:${dbPassword}@${dbAddress}`, { useMongoClient: true })
+const connectToDb = async (dbUser, dbPassword, dbAddress) => {
+  const str = dbUser && dbPassword ? `mongodb://${dbUser}:${dbPassword}@${dbAddress}` : `mongodb://${dbAddress}`
+  return mongoose.connect(str, { useMongoClient: true })
+}
 
 export default connectToDb
