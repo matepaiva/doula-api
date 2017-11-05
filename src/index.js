23e866e91c6b6c5cadfcf462e225e2f728aea853
Matheus Paiva <mmccpp@gmail.com>
2017-11-05 15:47:39 -0200

    added log info about environment to index

--- src/index.js
+++ src/index.js
@@ -3,10 +3,13 @@ import emoji from 'node-emoji'
 import app from './app'
 import { connectToDb } from './model'
 
-const { DB_USER, DB_PASSWORD, DB_ADDRESS, PORT = 8080 } = process.env
+const { DB_USER, DB_PASSWORD, DB_ADDRESS, PORT = 8080, NODE_ENV } = process.env
+
+console.log(`  ${emoji.get('heart')} Environment: ${NODE_ENV}  ${emoji.get('heart')}`)
+console.log(`  ${emoji.get('heart')} Connecting to Database (${DB_ADDRESS})...  ${emoji.get('heart')}`)
 
 connectToDb(DB_USER, DB_PASSWORD, DB_ADDRESS)
   .then(() => {
-    console.log(`  ${emoji.get('heart')} Database connected  ${emoji.get('heart')}`) // eslint-disable-line no-console
+    console.log(`  ${emoji.get('heart')} Database connected!  ${emoji.get('heart')}`) // eslint-disable-line no-console
     app.listen(PORT, () => console.log(`  ${emoji.get('heart')} Running on  /:${PORT}  ${emoji.get('heart')}`)) // eslint-disable-line no-console
   })
